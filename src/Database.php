efed55cce3f52cdc692ca2a80ccfc9b8459f2a37
Marina Tanaka <huttenloucher@gmail.com>
2023-10-03 11:25:58 -0300

    update semantico

--- src/Database.php
+++ src/Database.php
@@ -1,21 +1,14 @@
 <?php
-// Namespace para o composer. 
 namespace ExemploCrudPoo;
 
-// 2ª versão - (use).
 use PDO, Exception;
 
-// Configurando a conexão com o banco atraves de propriedades estaticas. 
 abstract class Database {
     private static string $servidor = "localhost";
     private static string $usuario = "root";
     private static string $senha = "";
     private static string $banco = "vendas_php-poo";
 
-    // 1ª versão - Classes da propria linguagem precisam da (\).
-    // private static \PDO $conexao;
-
-    // 2ª versão - (use)
     private static PDO $conexao;
 
     // Método de conexão ao banco utilizando o self
