d60c10c417cd926a9dbf2c72cc447d3017e23a86
Marina Tanaka <huttenloucher@gmail.com>
2023-10-03 10:32:26 -0300

    ajuste semantico

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD PHP e SQL</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="container">
         <h1>Sistema Vendas - CRUD PHP e SQL</h1>
@@ -20,4 +22,5 @@
     </div>
 
 </body>
+
 </html>
